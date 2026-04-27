6c618d926952f7add7e9d9e08c3edea2ebd73462
paulbanse <paul.banse@ens-rennes.fr>
2026-04-27 10:43:33 +0200

    adding the figures

---
+++ module_ESN.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+class EchoStateNetwork:
+
+    def __init__(self,
+                n,
+                avg_number_of_edges=2,
+                alpha = 0.1, 
+                proxy_discard = 50,
+                goal_discard = 100, 
+                measure_time = 50, 
+                seed = None, 
+                number_of_action_nodes =16):
+        """
+        Initialize the Echo State Network (ESN) ensuring full percolation of action nodes.
+        This version of the ESN ensures that all action nodes are connected to each other, allowing for full percolation.
+
+        Parameters
+        ----------
+        n : int
+            Number of nodes in the reservoir.
+        avg_number_of_edges : float, optional
+            Average number of edges per node, by default 2.
+        action_nodes : list, optional
+            List of nodes that should be connected to each other, by default [1].
+        alpha : float, between 0 and 1, optional
+            Low pass filter parameter, lower value increases oscillation periods.
+        """
+        self.spectral_radius = None
+        self.n = n
+        self.goal = [n-1]
+        assert(n> number_of_action_nodes + 2)
+        self.number_of_action_nodes = number_of_action_nodes
+        self.action_nodes = [k for k in range(number_of_action_nodes)]
+        
+        self.avg_number_of_edges = avg_number_of_edges
+        if seed is None:
+            seed = np.random.randint(0, 1000000)
+        self.seed = seed
+        self.rng = np.random.default_rng(seed)
+        self.W = self._initialize_useful_reservoir()
+        self.connectivity = np.where(self.W !=0, 1, 0)
+        self.state = np.zeros(n)
+        self.alpha = alpha
+        self.proxy_discard = proxy_discard
+        self.goal_discard = goal_discard
+        self.measure_time = measure_time
+        self.endstep = max(self.proxy_discard, self.goal_discard)+ self.measure_time
+
+
+    
+    def _initialize_useful_reservoir(self):
+        """
+        Initialize the reservoir connectivity matrix with a specific structure.
+        Parameters
+        -------
+        np.ndarray
+            Initialized connectivity matrix with specific structure.
+        """
+        W = np.zeros((self.n, self.n))
+        unconnected_nodes = [i for i in range(self.n) if i not in self.action_nodes]
+        connected_nodes = list(self.action_nodes)
+        while unconnected_nodes:
+            # Randomly select an action node to connect to
+            connect_to = self.rng.choice(unconnected_nodes)
+            connect_from = self.rng.choice(connected_nodes)
+            W[connect_to,connect_from] = 2*self.rng.random()-1
+            unconnected_nodes.remove(connect_to)
+            connected_nodes.append(connect_to)
+        
+        total_posssible_edges = self.n * (self.n - 1) - self.n + len(self.action_nodes)
+        probability_of_edge = (self.avg_number_of_edges-1)*self.n / total_posssible_edges
+
+        for k in range(self.n):
+            for j in range(self.n):
+                if self.rng.random() < probability_of_edge and W[k, j] == 0 and k != j:
+                    W[k, j] = self.rng.random() - 0.5
+        self.spectral_radius = np.max(np.abs(np.linalg.eigvals(W)))
+        return W
+
+    def step(self):
+        """
+        Update the reservoir state for one time step.
+
+        Parameters
+        ----------
+        agent_nodes : array-like
+            Indices of nodes controlled by the agent.
+        agent_values : array-like
+            Fixed values (-1, 0, 1) assigned to the agent-controlled nodes.
+
+        Returns
+        -------
+        np.ndarray
+            Updated reservoir state.
+        """
+        new_state = (1-self.alpha) * self.state + self.alpha* np.tanh(self.W @ self.state)
+        self.state = new_state
+        
+        return self.state
+
+    def run(self, agent_values):
+
+        collected_states = []
+        self.state = np.zeros(self.n)
+        self.state[self.action_nodes ] = agent_values  # Override controlled nodes
+
+        for t in range(self.endstep):
+            collected_states.append(self.state.copy())
+            self.step()
+        return np.array(collected_states)
+    
+    def distance(self, nodes1, nodes2):
+        """
+        Compute the distance between objective and proxy nodes.
+
+        Parameters
+        ----------
+        states : np.ndarray
+            Array of reservoir states over time.
+        obj_nodes : array-like
+            Indices of objective nodes.
+        proxy_nodes : array-like
+            Indices of proxy nodes.
+
+        Returns
+        -------
+        np.ndarray
+            Array of distances between objective and proxy nodes.
+        """
+        self.state = np.zeros(self.n)
+        self.state[nodes1] = 1
+        for k in range(self.n):
+            if any(self.state[nodes2]):
+                return k
+            self.state = np.where(self.connectivity @ self.state !=0, 1, 0) 
+        
+        return self.n+1
+    
+    def reset_seed(self):
+        self.rng = np.random.default_rng(self.seed)
+
+    def get_base_action_value(self, size = None):
+        """
+        Compute the action values if all actions are neutral according to a beta distribution between -1 and 1 of parameter 2 and 2
+        """
+        a, b = (2,2)
+        return 2*self.rng.beta( a, b, size)-1
+
+    def get_directed_action_value(self, action_value , target_node, actionsize = None):
+        """
+        Compute the action values if one action is targetted to be enforced (+1) or reduced (-1) 
+        """
+        params = [(2,2),(20,2),(2,20)]
+        A_vals = self.get_base_action_value(actionsize)
+        a,b = params[action_value]
+        A_vals[target_node]= 2*self.rng.beta( a, b)-1
+        return A_vals
+
+def compute_averages(states, obj_nodes, proxy_nodes):
+    """
+    Compute time-averaged values for objective and proxy nodes.
+
+    Parameters
+    ----------
+    states : np.ndarray
+        Array of reservoir states over time.
+    obj_nodes : array-like
+        Indices of objective nodes.
+    proxy_nodes : array-like
+        Indices of proxy nodes.
+
+    Returns
+    -------
+    tuple
+        Averages of objective and proxy nodes.
+    """
+    avg_obj = np.mean(states[:, obj_nodes])
+    avg_proxy = np.mean(states[:, proxy_nodes])
+    return avg_obj, avg_proxy
+
